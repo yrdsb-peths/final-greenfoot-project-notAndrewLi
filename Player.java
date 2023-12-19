9e2d7b176697f94251cef670508ed198c7232d81
Andrew <andrew.li2252@gmail.com>
2023-12-19 13:24:33 -0500

    Knight Sword Swing
    Knight can play a sword swinging animation by clicking

--- Player.java
+++ Player.java
@@ -16,6 +16,8 @@ public class Player extends Actor
     GreenfootImage[] idleRight = new GreenfootImage[4];
     GreenfootImage[] runLeft = new GreenfootImage[4];
     GreenfootImage[] runRight = new GreenfootImage[4];
+    GreenfootImage[] swingLeft = new GreenfootImage[4];
+    GreenfootImage[] swingRight = new GreenfootImage[4];
     boolean facingRight = true;
     boolean idle = true;
     SimpleTimer animationTimer = new SimpleTimer();
@@ -23,19 +25,22 @@ public class Player extends Actor
      * Makes a left and right facing knight depending on which way the player is facing.
      */
     public Player (){
-        for(int i = 0; i < idleLeft.length; i++){
+        for(int i = 0; i < 4; i++){
             idleLeft[i] = new GreenfootImage("images/knightIdle/idle" + (i+1) + ".png");
             idleLeft[i].scale(150, 150);
             idleLeft[i].mirrorHorizontally();
             idleRight[i] = new GreenfootImage("images/knightIdle/idle" + (i+1) + ".png");
             idleRight[i].scale(150, 150);
-        }
-        for(int i = 0; i < runLeft.length; i++){
             runLeft[i] = new GreenfootImage("images/knightRun/run" + (i+1) + ".png");
             runLeft[i].scale(150, 150);
             runLeft[i].mirrorHorizontally();
             runRight[i] = new GreenfootImage("images/knightRun/run" + (i+1) + ".png");
             runRight[i].scale(150, 150);
+            swingRight[i] = new GreenfootImage("images/knightSwing/swing" + (i+1) + ".png");
+            swingRight[i].scale(150, 150);
+            swingLeft[i] = new GreenfootImage("images/knightSwing/swing" + (i+1) + ".png");
+            swingLeft[i].mirrorHorizontally();
+            swingLeft[i].scale(150, 150);
         }
         animationTimer.mark();
         setImage(idleRight[1]);
@@ -51,7 +56,11 @@ public class Player extends Actor
          else if(Greenfoot.isKeyDown("d")){
             facingRight = true;
             move(5);
-        } else{
+        }
+        else if(Greenfoot.mouseClicked(null)){
+            swingSword();
+        }
+        else{
             idle = true;
         }
         animateKnight();
@@ -79,5 +88,19 @@ public class Player extends Actor
             }
         }
         imageIndex = (imageIndex + 1) % idleRight.length;
+    }
+    public void swingSword(){
+        if(facingRight){
+            for(int i = 0; i < swingRight.length; i++){
+                setImage(swingRight[i]);
+                Greenfoot.delay(1);
+            }
+        } else{
+            for(int i = 0; i < swingLeft.length; i++){
+                setImage(swingLeft[i]);
+                Greenfoot.delay(1);
+            }
+        }
+        setImage(idleLeft[1]);
     }
 }
